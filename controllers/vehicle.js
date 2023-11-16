85e6186274ae5cff307ec13d054b4a6526fbb1f5
asrithadaida <137849928+asrithadaida@users.noreply.github.com>
2023-11-15 19:46:14 -0600

    made changes to create,update,delete vehicles

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -1,3 +1,4 @@
+const vehicle = require('../models/vehicle');
 var Vechicle = require('../models/vehicle');
 // List of all Costumes
 exports.vehicle_list = async function(req, res) {
@@ -11,9 +12,17 @@ exports.vehicle_list = async function(req, res) {
     }
 };
 // for a specific Costume.
-exports.vehicle_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Vechicle detail: ' + req.params.id);
+exports.vehicle_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await Vehicle.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }      
 };
+
 // Handle Costume create on POST.
 exports.vehicle_create_post = async function(req, res) {
     console.log(req.body)
@@ -35,13 +44,38 @@ exports.vehicle_create_post = async function(req, res) {
     }
 };
 // Handle Costume delete form on DELETE.
-exports.vehicle_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Vechicle delete DELETE ' + req.params.id);
+exports.vehicle_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await vehicle.findByIdAndDelete( req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }     
 };
 // Handle Costume update form on PUT.
-exports.vehicle_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Vechicle update PUT' + req.params.id);
+exports.vehicle_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await vehicle.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.VehicleName) toUpdate.VehicleName = req.body.VehicleName;
+    if(req.body.price) toUpdate.price = req.body.price;
+    if(req.body.Model) toUpdate.Model = req.body.Model;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
 };
+
+
 
 // VIEWS
 // Handle a show all view
@@ -53,5 +87,54 @@ exports.vehicle_view_all_Page = async function(req, res) {
     catch(err){
         res.status(500);
         res.send(`{"error": ${err}}`);
+    }
+};
+
+// Handle a show one view with id specified by query
+exports.vehicle_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+        result = await Vechicle.findById( req.query.id)
+        res.render('vehicledetail', { title: 'Vehicle Detail', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+exports.vehicle_create_Page =  function(req, res) {
+    console.log("create view")
+    try{
+        res.render('vehiclecreate', { title: 'Vehicle Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+exports.vehicle_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await Vechicle.findById(req.query.id)
+        res.render('vehicleupdate', { title: 'Vehicle Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+    
+exports.vehicle_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+        result = await Vechicle.findById(req.query.id)
+        res.render('vehicledelete', { title: 'Vehicle Delete', toShow:
+        result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
     }
 };
--- routes/Vehicle.js
+++ routes/Vehicle.js
@@ -8,4 +8,18 @@ var router = express.Router();
 });*/
 router.get('/', vehicle_controlers.vehicle_view_all_Page );
 
+// // GET request for one costume.
+router.get('/vehicle/:id', vehicle_controlers.vehicle_detail);
+
+router.get('/detail', vehicle_controlers.vehicle_view_one_Page);
+
+router.get('/create', vehicle_controlers.vehicle_create_Page);
+
+router.get('/update', vehicle_controlers.vehicle_update_Page);
+
+router.get('/delete', vehicle_controlers.vehicle_delete_Page);
+
 module.exports = router;
+
+
+
