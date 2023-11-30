cb8fe54dbb7ad41e06c2ab59db1568ea35d6fafd
asrithadaida <137849928+asrithadaida@users.noreply.github.com>
2023-11-29 19:38:38 -0600

    modified

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const vehicleSchema = mongoose.Schema({
-    VehicleName: String,
-    price: Number,
+    VehicleName: {type:String, requried: 'Vehicle name is requried'},
+    price: {type:Number, Minimum: 40000, maximum:300000},
     Model: String
 })
 module.exports = mongoose.model("Vehicle",vehicleSchema);
--- routes/Vehicle.js
+++ routes/Vehicle.js
@@ -2,6 +2,17 @@ var express = require('express');
 const vehicle_controlers= require('../controllers/vehicle');
 var router = express.Router();
 
+
+//A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
 /* GET home page. */
 /*router.get('/', function(req, res, next) {
   res.render('Vehicle', { title: 'Search for Vehicles' });
@@ -13,11 +24,11 @@ router.get('/vehicle/:id', vehicle_controlers.vehicle_detail);
 
 router.get('/detail', vehicle_controlers.vehicle_view_one_Page);
 
-router.get('/create', vehicle_controlers.vehicle_create_Page);
+router.get('/create', secured, vehicle_controlers.vehicle_create_Page);
 
-router.get('/update', vehicle_controlers.vehicle_update_Page);
+router.get('/update', secured,  vehicle_controlers.vehicle_update_Page);
 
-router.get('/delete', vehicle_controlers.vehicle_delete_Page);
+router.get('/delete', secured, vehicle_controlers.vehicle_delete_Page);
 
 module.exports = router;
 
